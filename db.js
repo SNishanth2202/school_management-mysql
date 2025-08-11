46ffe878ca5d45a824950b8c735724799a5ff850
SNishanth2202 <your.email@example.com>
2025-08-11 14:08:56 +0530

    completed

--- db.js
+++ db.js
@@ -7,6 +7,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME || 'school_management',
+  port: process.env.DB_PORT || 3306,
   waitForConnections: true,
 });
 
